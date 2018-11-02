28a1849bb6bb76ef68ec76cee6d7d57d0dd758ae
Carnelas <carnelas@gmail.com>
2018-11-02 15:23:21 +0100

    preparada para docker

--- service/app.js
+++ service/app.js
@@ -2,15 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+let connectRetry = function() {
+  return mongoose.connect('mongodb://mongodb:27017/message', {useNewUrlParser: true}, function(err) {
+    if (err) {
+      console.error('Error connecting to mongo - retrying in 5 seconds', err);
+      setTimeout(connectRetry, 5000);
+    } else {
+      console.log("connected to Mongo!!")
+    }
+  });
+};
+connectRetry();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const message = require("./router/message");
-app.use("/message", message);
-
-const sendMessage = require('./routes/sendMessage')
+const sendMessage = require('./routes/sendMessages')
 const getMessages = require('./routes/getMessages');
-app.use('/message', sendMessage)
+app.use('/messages', sendMessage)
 app.use('/messages', getMessages);
 
 app.listen(process.env.SERVICE_PORT || 9001, function() {
